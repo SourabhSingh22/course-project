686dced83b5cac56bef6f15ba73e9e7cb3c36faf
SourabhSingh22 <sourabhsingh22112003@gmail.com>
2024-09-07 13:21:04 +0530

    change all over project

--- script.js
+++ script.js
@@ -1,206 +1,112 @@
-const checkboxes = document.querySelectorAll(' input[type="checkbox"]');
-const checkBox = document.getElementById("checkBox-one");
-const counter = document.getElementById('counter');
-
-const increasingLine = document.getElementById("increasing-line");
-const increasingP = document.getElementById("increasing-p");
-
-
-// Increasing Your progress:-
-
-const countData = document.getElementById("count-data");
-
-function updateCountData() {
-    const checkedData = document.querySelectorAll('input[type="checkbox"]:checked').length;
-    countData.textContent = `${checkedData}/10`;
-}
-
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', updateCountData);
-});
-
-// 
-
-
-// 1st button k liye
-
-function updateCounter() {
-
-    increasingLine.classList.add("inc-line");
-    const checkedCount = document.querySelectorAll(' input[type="checkbox"]:checked').length;
-    counter.textContent = `${checkedCount} /3`;
-
-
-    increasingP.textContent = `${checkedCount * 10}% complete`;
-
-    increasingLine.style.width = `${checkedCount * 10}%`;
-
-}
-
-checkboxes.forEach(checkBox => {
-    checkBox.addEventListener('change', updateCounter);
-
-});
-
-// 
-
-// one down-area:-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const arrow = document.getElementById("down-arrow");
-    const downArea = document.getElementById("down-area");
-    // const arrowDiv = document.getElementById("arrow");
-
-    // downArea.style.display ="none";
-
-    function toggleDownAreaOne() {
-
-        if (arrow.src.includes("down-arrow.png")) {
-            arrow.src = "icon/up-arrow.png";
-            downArea.classList.add('hidden');
-        } else {
-            arrow.src = "icon/down-arrow.png";
-            downArea.classList.remove('hidden');
-        }
-    }
-    arrow.addEventListener('click', toggleDownAreaOne);
-    // arrowDiv.addEventListener('click', toggleDownArea);
-});
-
-// 2nd button k liye
-
-const checkboxesTwo = document.querySelectorAll('input[type="checkbox"]');
-const checkBoxTwo = document.getElementById("checkBox-two");
+// All Ids 
+const counterOne = document.getElementById("counter-one");
 const counterTwo = document.getElementById("counter-two");
-const downArrowTwo = document.getElementById("down-arrow-two");
-
-function updateCounterTwo() {
-
-    const checkedCountTwo = document.querySelectorAll('input[type="checkbox"]:checked').length;
-    counterTwo.textContent = `${checkedCountTwo} /3`;
-
-    // increasingLine.classList.add("inc-line");
-    // increasingP.textContent = `${checkedCountTwo * 10}% complete`;
-    // increasingLine.style.width = `${checkedCountTwo * 10}%`;
-}
-
-checkboxesTwo.forEach(checkBoxTwo => {
-    checkBoxTwo.addEventListener('change', updateCounterTwo);
-});
-
-//
-
-// Two down-area:-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const arrowTwo = document.getElementById("down-arrow-two");
-    const downAreaTwo = document.getElementById("down-area-two");
-
-    function toggleDownAreaTwo() {
-
-        if (arrowTwo.src.includes("down-arrow.png")) {
-            arrowTwo.src = "icon/up-arrow.png";
-            downAreaTwo.classList.add('hidden');
-        } else {
-            arrowTwo.src = "icon/down-arrow.png";
-            downAreaTwo.classList.remove('hidden');
-        }
-    }
-    arrowTwo.addEventListener('click', toggleDownAreaTwo);
-
-});
-
-// 
-
-// 3rd button k liye
-
-const checkboxesThree = document.querySelectorAll('input[type="checkbox"]');
-const checkBoxThree = document.getElementById("checkBox-three");
 const counterThree = document.getElementById("counter-three");
-const downArrowThree = document.getElementById("down-arrow-three");
-
-function updateCounterThree() {
-
-    const checkedCountThree = document.querySelectorAll('input[type="checkbox"]:checked').length;
-    counterThree.textContent = `${checkedCountThree} /3`;
-
-    // increasingLine.classList.add("inc-line");
-
-    // increasingP.textContent = `${checkedCountThree * 10}% complete`;
-    // increasingLine.style.width = `${checkedCountThree * 10}%`;
-}
-
-checkboxesThree.forEach(checkBoxThree => {
-    checkBoxThree.addEventListener('change', updateCounterThree);
-});
-// 
-
-
-// Three down-area:-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const arrowThree = document.getElementById("down-arrow-three");
-    const downAreaThree = document.getElementById("down-area-three");
-
-    function toggleDownAreaThree() {
-
-        if (arrowThree.src.includes("down-arrow.png")) {
-            arrowThree.src = "icon/up-arrow.png";
-            downAreaThree.classList.add('hidden');
-        } else {
-            arrowThree.src = "icon/down-arrow.png";
-            downAreaThree.classList.remove('hidden');
-        }
-    }
-    arrowThree.addEventListener('click', toggleDownAreaThree);
-    // arrowDiv.addEventListener('click', toggleDownArea);
-});
-
-//
-
-
-// 4rd button k liye
-
-const checkboxesFourth = document.querySelectorAll('input[type="checkbox"]');
 const counterFourth = document.getElementById("counter-fourth");
-const downArrowFourth = document.getElementById("down-arrow-fourth");
+// Select all inputs
+const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+// 
+const countNumber = document.getElementById("count-number");
+const countPercentage = document.getElementById("count-percentage");
+const increasingLine = document.getElementById("increasing-line");
 
-function updateCounterFourth() {
+// update counts, percentage and increasingLine
+function updateCounts() {
+    let checkedCountFirst = document.querySelectorAll('#first-input-section input[type="checkbox"]:checked').length;
+    let checkedCountSecond = document.querySelectorAll('#second-input-section input[type="checkbox"]:checked').length;
+    let checkedCountThird = document.querySelectorAll('#third-input-section input[type="checkbox"]:checked').length;
+    let checkedCountFourth = document.querySelectorAll('#fourth-input-section input[type="checkbox"]:checked').length;
 
-    const checkedCountFourth = document.querySelectorAll('input[type="checkbox"]:checked').length;
-    counterFourth.textContent = `${checkedCountFourth} /3`;
+    increasingLine.classList.add('increase');
 
-    // increasingLine.classList.add("inc-line");
-    // increasingP.textContent = `${checkedCountFourth * 10}% complete`;
-    // increasingLine.style.width = `${checkedCountFourth * 10}%`;
+    let totalCheckedCount = checkedCountFirst + checkedCountSecond + checkedCountThird + checkedCountFourth;
+    let totalCheckboxes = 10; 
+
+    // 
+    countNumber.textContent = `${totalCheckedCount} /${totalCheckboxes}`;
+    countPercentage.textContent = `${(totalCheckedCount / totalCheckboxes) * 100}% complete`;
+    increasingLine.style.width = `${(totalCheckedCount / totalCheckboxes) * 100}%`;
+
+    //
+    counterOne.textContent = `${checkedCountFirst} /3`;
+    counterTwo.textContent = `${checkedCountSecond} /3`;
+    counterThree.textContent = `${checkedCountThird} /3`;
+    counterFourth.textContent = `${checkedCountFourth} /1`;
+
+
 }
 
-checkboxesFourth.forEach(checkboxesFourth => {
-    checkboxesFourth.addEventListener('change', updateCounterFourth);
-});
-
-//
-
-// Fourth down-area:-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const arrowFourth = document.getElementById("down-arrow-fourth");
-    const downAreaFourth = document.getElementById("down-area-fourth");
-
-    function toggleDownAreaFourth() {
-
-        if (arrowFourth.src.includes("down-arrow.png")) {
-            arrowFourth.src = "icon/up-arrow.png";
-            downAreaFourth.classList.add('hidden');
-        } else {
-            arrowFourth.src = "icon/down-arrow.png";
-            downAreaFourth.classList.remove('hidden');
-        }
-    }
-    arrowFourth.addEventListener('click', toggleDownAreaFourth);
-    // arrowDiv.addEventListener('click', toggleDownArea);
-});
-
 // 
+checkBoxes.forEach(checkbox => {
+    checkbox.addEventListener('change', updateCounts);
+});
 
+// Down-area-one
+const downAreaOne = document.getElementById("down-area-one");
+const oneDayText = document.getElementById("one-day-text");
+const arrowOne = document.getElementById("arrow-one");
+
+arrowOne.addEventListener('click', function() {
+    if (downAreaOne.style.display === 'block' || downAreaOne.style.display === '') {
+        oneDayText.style.color = 'white';
+        downAreaOne.style.display = 'none';
+        arrowOne.classList.remove('rotate-180');
+    } else {
+        oneDayText.style.color = "skyblue";
+        downAreaOne.style.display = 'block';
+        arrowOne.classList.add('rotate-180');
+    }
+});
+
+// Down-area-two
+const downAreaTwo = document.getElementById("down-area-two");
+const twoDayText = document.getElementById("two-day-text");
+const arrowTwo = document.getElementById("arrow-two");
+
+arrowTwo.addEventListener('click', function() {
+    if (downAreaTwo.style.display === 'block' || downAreaTwo.style.display === '') {
+        twoDayText.style.color = 'white';
+        downAreaTwo.style.display = 'none';
+        arrowTwo.classList.remove('rotate-180');
+    } else {
+        twoDayText.style.color = 'skyblue';
+        downAreaTwo.style.display = 'block';
+        arrowTwo.classList.add('rotate-180');
+    }
+});
+
+// Down-area-three
+const downAreaThree = document.getElementById("down-area-three");
+const threeDayText = document.getElementById("three-day-text");
+const arrowThree = document.getElementById("arrow-three");
+
+arrowThree.addEventListener('click', function() {
+    if (downAreaThree.style.display === 'block' || downAreaThree.style.display === '') {
+        threeDayText.style.color = 'white';
+        downAreaThree.style.display = 'none';
+        arrowThree.classList.remove('rotate-180');
+    } else {
+        threeDayText.style.color = 'skyblue';
+        downAreaThree.style.display = 'block';
+        arrowThree.classList.add('rotate-180');
+    }
+});
+
+// Down-area-four
+const downAreaFour = document.getElementById("down-area-four");
+const fourDayText = document.getElementById("four-day-text");
+const arrowFour = document.getElementById("arrow-four");
+
+arrowFour.addEventListener('click', function() {
+    if (downAreaFour.style.display === 'block' || downAreaFour.style.display === '') {
+        fourDayText.style.color = 'white';
+        downAreaFour.style.display = 'none';
+        arrowFour.classList.remove('rotate-180');
+    } else {
+        fourDayText.style.color = 'skyblue';
+        downAreaFour.style.display = 'block';
+        arrowFour.classList.add('rotate-180');
+    }
+
+});
 
